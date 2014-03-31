476bee20333c2f61f69275efcef58676e183fd3d
pukoren <koren@pukogames.com>
2014-03-31 14:56:24 +0200

    Basic main program for Irrlicht

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,36 @@
-int main(int argc, char** argv){
+#include <irrlicht/irrlicht.h>
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
 
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+int main(int argc, char** argv){
+    IrrlichtDevice *device = createDevice(video::EDT_SOFTWARE,
+                                          dimension2d<u32>(640, 480), 16,
+                                          false, false, false, 0);
+    device->setWindowCaption(L"Shaders and lights - Irrlicht");
+
+    if (!device)
+        return 1;
+
+    IVideoDriver* driver = device->getVideoDriver();
+    ISceneManager* smgr = device->getSceneManager();
+    IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+    while(device->run()){
+        driver->beginScene(true, true, SColor(255,100,101,140));
+        smgr->drawAll();
+        guienv->drawAll();
+        driver->endScene();
+    }
+
+    device->drop();
 	return 0;
 }
